5c437f03a0828f424b5e16b1e23552c4c2ab83b6
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-05-12 10:58:41 +0200

    Terminado

---
+++ Ejercicio.py
@@ -0,0 +1,82 @@
+from abc import ABC, abstractmethod
+from flask import Flask, request, render_template
+
+class Pais(ABC):
+    def __init__(self, nombre, capital):
+        self.nombre = nombre
+        self.capital = capital
+
+    @abstractmethod
+    def descripcion(self):
+        pass
+
+class Mexico(Pais):
+    def __init__(self, nombre, capital, idioma):
+        super().__init__(nombre, capital)
+        self.idioma = idioma
+
+    def descripcion(self):
+        return(f"México es un país de América del Norte, cuya capital es {self.capital} y su idioma oficial es el {self.idioma}.")
+
+class Japon(Pais):
+    def __init__(self, nombre, capital, moneda):
+        super().__init__(nombre, capital)
+        self.moneda = moneda
+
+    def descripcion(self):
+        return(f"Japón es un país de Asia, cuya capital es {self.capital} y su moneda oficial es el {self.moneda}.")
+
+class Espana(Pais):
+    def __init__(self, nombre, capital, region):
+        super().__init__(nombre, capital)
+        self.region = region
+
+    def descripcion(self):
+        return(f"España es un país de Europa, cuya capital es {self.capital} y se encuentra en la región de {self.region}.")
+
+
+app = Flask(__name__,template_folder='html')
+
+@app.route("/")
+def Paises():
+    return render_template("start_Paises.html")
+
+@app.route("/Paises", methods=['GET', 'POST'])
+def mostrar_Paises():
+    if request.method == 'POST':
+        pais = request.form.get("Pais")
+ # Obtener la Pais seleccionado por el usuario
+    if pais == "Mexico":
+        Pais_ingresado = {
+        'nombre': 'México',
+        'capital': 'Ciudad de México',
+        'idioma': 'Español',
+        'moneda': 'Peso mexicano',
+        'region': 'América',
+        'descripcion': Mexico("México", "Ciudad de México", "Español").descripcion(),
+    }
+    elif pais == "Japón":
+        Pais_ingresado = {
+        'nombre': 'Japón',
+        'capital': 'Tokio',
+        'idioma': 'Japonés',
+        'moneda': 'Yen',
+        'region': 'Asia',
+        'descripcion': Japon("Japón", "Tokio", "Japonés").descripcion(),
+    }
+    elif pais == "España":
+        Pais_ingresado = {
+        'nombre': 'España',
+        'capital': 'Madrid',
+        'idioma': 'Español',
+        'moneda': 'Euros',
+        'region': 'Europa',
+        'descripcion': Espana("España", "Madrid", "Europa").descripcion(),
+    }
+ # Insertar el código aquí
+        
+ # Renderizar la página de Paises con el Pais seleccionado
+    return render_template("Paises.html", Pais=Pais_ingresado)
+
+if __name__ == '__main__':
+   app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-"# FirstAssesmentPython" 
+# FirstAssesmentPython
+
+### Instrucciones de instalación:
+
+- Clonar el repositorio:
+
+  - `git clone https://github.com/JonPiniesEcheguren/FirstAssesmentPython.git` 
+
+- Crear y activar el entorno virtual:
+
+  - Versión Python 3.11.2
+
+  - Windows
+
+    - `python -m venv env` 
+    - `env\scripts\activate.bat` (Windows command line)
+    - `env\scripts\activate.ps1` (Windows PowerShell)
+
+  - MacOs / Linux
+
+    - `python3 -m venv env`
+
+    - `source env/bin/activate`
+
+- Instalar las librerias
+  - `pip install -r requirements.txt`
+
+- Instrucciones de ejecución
+  - `py Ejercicio.py`
+
+### Colaboradores:
+
+- Jon Pinies Echeguren
+
+Ejercicios de carácter puramente educativo.
+
+## Los programas presentes en este repositorio fueron creados para resolver los siguientes problemas: 
+
